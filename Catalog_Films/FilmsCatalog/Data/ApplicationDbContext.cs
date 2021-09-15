994f9de4aa2e08109b5c8fb294a8e0ccbe6d0f28
Artyom <artyom.kalughin@mail.ru>
2021-09-16 02:02:50 +0700

    Создал класс Actor.cs и коллекцию Actors в БД.

--- Catalog_Films/FilmsCatalog/Data/ApplicationDbContext.cs
+++ Catalog_Films/FilmsCatalog/Data/ApplicationDbContext.cs
@@ -13,5 +13,12 @@ namespace FilmsCatalog.Data
             : base(options)
         {
         }
+
+        public DbSet<Actor> Actors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
