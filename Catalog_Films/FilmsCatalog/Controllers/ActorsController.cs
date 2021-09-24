2b8bfb49a4dcc943d68bef66a828158110905126
Artyom <artyom.kalughin@mail.ru>
2021-09-24 23:40:56 +0700

    Поменял название файла на ActorCreateViewModel из-за опечатки. Написал методы GET/POST Edit в ActorsController, создал файл ActorEditViewModel и доработал его. Создал представление Edit.cshtml для ActorsController(добавил поля для редактирования информации об актёре и кнопку "Сохранить")

--- Catalog_Films/FilmsCatalog/Controllers/ActorsController.cs
+++ Catalog_Films/FilmsCatalog/Controllers/ActorsController.cs
@@ -104,5 +104,85 @@ namespace FilmsCatalog.Controllers
             return View(actor);
         }
 
+        // GET: Actors/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var actor = await this._context.Actors
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            var model = new ActorEditViewModel
+            {
+                Name = actor.Name,
+                SurName = actor.SurName,
+                Height = actor.Height,
+                Date_of_birth = actor.Date_of_birth,
+                Number_of_films = actor.Number_of_films
+            };
+
+            return this.View(model);
+        }
+
+        // POST: Films/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid? id, ActorEditViewModel model)
+        {
+            if (id == null)
+            {
+                return this.NotFound();
+            }
+
+            var actor = await this._context.Actors
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (actor == null)
+            {
+                return this.NotFound();
+            }
+
+            var fileName = Path.GetFileName(ContentDispositionHeaderValue.Parse(model.Photo.ContentDisposition).FileName.Trim('"'));
+            var fileExt = Path.GetExtension(fileName);
+            if (!AllowedExtensions.Contains(fileExt))
+            {
+                ModelState.AddModelError(nameof(model.Photo), "This file type is prohibited");
+            }
+
+            if (this.ModelState.IsValid)
+            {
+                actor.Name = model.Name;
+                actor.SurName = model.SurName;
+                actor.Height = model.Height;
+                actor.Date_of_birth = model.Date_of_birth;
+                actor.Number_of_films = model.Number_of_films;
+
+                if (model.Photo != null)
+                {
+                    var photoPath = Path.Combine(hostingEnvironment.WebRootPath, "attachments", actor.Id.ToString("N") + fileExt);
+                    actor.Path = $"/attachments/{actor.Id:N}{fileExt}";
+                    using (var fileStream = new FileStream(photoPath, FileMode.CreateNew, FileAccess.ReadWrite, FileShare.Read))
+                    {
+                        await model.Photo.CopyToAsync(fileStream);
+                    }
+                }
+
+                await this._context.SaveChangesAsync();
+                return this.RedirectToAction("Index");
+            }
+
+            return this.View(model);
+        }
+
     }
 }
